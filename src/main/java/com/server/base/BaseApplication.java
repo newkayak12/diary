e5ceb58306d52832c45647350358dded2483440f
newkayak12 <newkayak12@gmail.com>
2022-05-23 20:23:06 +0900

    /

--- src/main/java/com/server/base/BaseApplication.java
+++ src/main/java/com/server/base/BaseApplication.java
@@ -1,13 +1,33 @@
 package com.server.base;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Slf4j
 public class BaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class, args);
+
+        log.error("-----------------------------------------------------------------------------");
+        log.error(" ⊂_ヽ ");
+        log.error("　 ＼＼ Λ＿Λ ");
+        log.error("　　 ＼( ‘ㅅ’ ) 두둠칫 ");
+        log.error("　　　 >　⌒ヽ ");
+        log.error("　　　/ 　 へ＼ ");
+        log.error("　　 /　　/　＼＼ ");
+        log.error("　　 ﾚ　ノ　　 ヽ_つ ");
+        log.error("　　/　/두둠칫 ");
+        log.error("　 /　/| ");
+        log.error("(　(ヽ ");
+        log.error("　|　|、＼ ");
+        log.error("　| 丿 ＼ ⌒) ");
+        log.error("　| |　　) / ");
+        log.error("`ノ )　　Lﾉ");
+        log.error("-----------------------------------------------------------------------------");
+
     }
 }
--- src/test/java/com/server/base/BaseApplicationTests.java
+++ src/test/java/com/server/base/BaseApplicationTests.java
@@ -1,34 +1,17 @@
 package com.server.base;
 
 import com.server.base.common.authorizations.TokenManager;
-import com.server.base.controller.TestController;
-import com.server.base.repository.dto.TestDto;
-import lombok.val;
-import org.junit.jupiter.api.BeforeEach;
+import com.server.base.repository.dto.UserDto;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockMultipartHttpServletRequest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @SpringBootTest
@@ -50,7 +33,8 @@ class BaseApplicationTests {
     @Test
     void aopTest() throws Exception {
 
-        TestDto v = TestDto.builder().id1("1").id2(1).id3(new Long(3)).id4(LocalDateTime.now().minusDays(1)).build();
+        UserDto v = new UserDto();
+//                String token = TokenManager.encrypt(v);
         String token = TokenManager.encrypt(v);
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(HttpHeaders.AUTHORIZATION, token);
