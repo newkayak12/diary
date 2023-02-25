bb3e3502088074b1eab125a839fd7cddfb026051
newkayak12 <newkayak12@gmail.com>
2023-02-25 21:40:18 +0900

    memory

--- src/main/java/com/server/diary/common/dto/PagingDto.java
+++ src/main/java/com/server/diary/common/dto/PagingDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.Objects;
@@ -14,24 +15,26 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PagingDto {
-    public void setStartDate(String startDate) {
-        if(Objects.isNull(startDate)||startDate.equals("")){
-            this.startDate=null;
-            return ;
-        }
-        this.startDate = LocalDate.parse(startDate);
-    }
-    public void setEndDate(String endDate) {
-        if(Objects.isNull(endDate)||endDate.equals("")){
-            this.endDate=null;
-            return ;
-        }
-        this.endDate = LocalDate.parse(endDate);
-    }
+//    public void setStartDate(String startDate) {
+//        if(Objects.isNull(startDate)||startDate.equals("")){
+//            this.startDate=null;
+//            return ;
+//        }
+//        this.startDate = LocalDate.parse(startDate);
+//    }
+//    public void setEndDate(String endDate) {
+//        if(Objects.isNull(endDate)||endDate.equals("")){
+//            this.endDate=null;
+//            return ;
+//        }
+//        this.endDate = LocalDate.parse(endDate);
+//    }
 
     private Integer page;
     private Integer limit;
     private String searchText;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
 }
